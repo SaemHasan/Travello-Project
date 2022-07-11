4147c2a03d4c2ccc51d265ac72f2b8d772df535f
rakin31 <shafqatrakin@gmail.com>
2022-07-11 23:05:57 +0600

    items added in listItem.jsx

--- frontend/travello-frontend/src/Explore/components/Home/List/ListItem/index.jsx
+++ frontend/travello-frontend/src/Explore/components/Home/List/ListItem/index.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+// import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
+
 
 const ListItem = ({
   item: { coverSrc, title, price, deliveryFee, serviceTime, rating },
-}) => (
-  <div className='listItem-wrap'>
-    <img src={coverSrc} alt='' />
-    <header>
-      <h4>{title}</h4>
-      <span>🌟{rating}</span>
-    </header>
-    <footer>
-      <p>
-        <b>{serviceTime}</b> <span> Delivery Fee ${deliveryFee}</span>
-      </p>
-      <p>
-        <b>${price}</b>
-      </p>
-    </footer>
-  </div>
-);
+}) =>{
+
+    const [places, setPlaces] = useState([
+    { id: 1, checked: false, label: 'Mountain' },
+    { id: 2, checked: false, label: 'Waterfall' },
+    { id: 3, checked: false, label: 'Forest' },
+  ]);
+
+    return(
+        <div className='listItem-wrap'>
+            <img src={coverSrc} alt=''/>
+            <header>
+                <h4>{title}</h4>
+                <span>🌟{rating}</span>
+            </header>
+            <footer>
+                <p>
+                    <b>{serviceTime}</b> <span> Delivery Fee ${deliveryFee}</span>
+                </p>
+                <p>
+                    <b>${price}</b>
+                </p>
+            </footer>
+        </div>
+    );
+};
+
 
 export default ListItem;

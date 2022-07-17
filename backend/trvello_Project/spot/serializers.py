f0c39e235c88b5890ac7c3e118ac8e262b2e3c8c
SaemHasan <1705027@ugrad.cse.buet.ac.bd>
2022-07-17 17:26:40 +0600

    all module serializers done

--- backend/trvello_Project/spot/serializers.py
+++ backend/trvello_Project/spot/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Place, PlaceRatingInfo, Spot, SpotType_Table, Spot_Type, User_Spot, Spot_Food, Spot_Activity, SpotRatingInfo
+from .models import Place, PlaceRatingInfo, Spot, SpotType_Table, Spot_Type, User_Spot, Spot_Food, Spot_Activity, \
+    SpotRatingInfo
 
 
 class PlaceRatingInfoSerializer(serializers.ModelSerializer):
@@ -7,15 +8,18 @@ class PlaceRatingInfoSerializer(serializers.ModelSerializer):
         model = PlaceRatingInfo
         fields = '__all__'
 
+
 class SpotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Spot
         fields = '__all__'
 
+
 class SpotTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Spot_Type
         fields = '__all__'
+
 
 class PlaceSerializer(serializers.ModelSerializer):
     spots = SpotSerializer(many=True)
@@ -23,25 +27,31 @@ class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = '__all__'
+
+
 class SpotType_TableSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpotType_Table
         fields = '__all__'
+
 
 class User_SpotSerializer(serializers.ModelSerializer):
     class Meta:
         model = User_Spot
         fields = '__all__'
 
+
 class Spot_FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Spot_Food
         fields = '__all__'
 
+
 class Spot_ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Spot_Activity
         fields = '__all__'
+
 
 class SpotRatingInfoSerializer(serializers.ModelSerializer):
     class Meta:

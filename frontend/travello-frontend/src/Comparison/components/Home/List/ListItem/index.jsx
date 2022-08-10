4911e47348f7c0ba011bf72e611ccaef5872e603
SaemHasan <1705027@ugrad.cse.buet.ac.bd>
2022-08-11 04:52:22 +0600

    run after merge

--- frontend/travello-frontend/src/Comparison/components/Home/List/ListItem/index.jsx
+++ frontend/travello-frontend/src/Comparison/components/Home/List/ListItem/index.jsx
@@ -1,56 +1,48 @@
 // import React from 'react';
-import React, { useEffect, useState } from 'react';
-import './styles.css';
-import ComparisonAPI from "../../../../ComparisonAPI";
-
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
 const ListItem = ({
   item: { coverSrc, title, desc, price, deliveryFee, serviceTime, rating },
-}) =>{
-
-
-    const [places, setPlaces] = useState([
-    { id: 1, checked: false, label: 'Mountain' },
-    { id: 2, checked: false, label: 'Waterfall' },
-    { id: 3, checked: false, label: 'Forest' },
+}) => {
+  const [places, setPlaces] = useState([
+    { id: 1, checked: false, label: "Mountain" },
+    { id: 2, checked: false, label: "Waterfall" },
+    { id: 3, checked: false, label: "Forest" },
   ]);
 
-    return(
-        // {{category} === "place" && (
-        <div className='listItem-wrap-comp' >
-
-
-            <ul>
-                <li>
-                    <img src={coverSrc} alt="adventure"/>
-      <span className="large-comp">
-          <div className="div-color-comp">
-
-              <img src={coverSrc} className="large-image-comp" alt="adventure" />
+  return (
+    // {{category} === "place" && (
+    <div className="listItem-wrap-comp">
+      <ul>
+        <li>
+          <img src={coverSrc} alt="adventure" />
+          <span className="large-comp">
+            <div className="div-color-comp">
+              <img
+                src={coverSrc}
+                className="large-image-comp"
+                alt="adventure"
+              />
               <div>
-          <p className="heading-des-comp"> Sylhet is a region with stunning natural beauty. Situated in the northeast of Bangladesh, this ancient township is rich in forest, minerals, and fisheries and is rich in natural beauty.</p>
-
-
-
+                <p className="heading-des-comp">
+                  {" "}
+                  Sylhet is a region with stunning natural beauty. Situated in
+                  the northeast of Bangladesh, this ancient township is rich in
+                  forest, minerals, and fisheries and is rich in natural beauty.
+                </p>
               </div>
+            </div>
+          </span>
+        </li>
+      </ul>
 
-
-          </div>
-
-      </span>
-                </li>
-
-
-  </ul>
-
-            <header>
-                <h4>{title}</h4>
-                <span>🌟{rating}</span>
-            </header>
-
-        </div>
-    );
+      <header>
+        <h4>{title}</h4>
+        <span>🌟{rating}</span>
+      </header>
+    </div>
+  );
 };
-
 
 export default ListItem;

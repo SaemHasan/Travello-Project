f01d67db3af0c097c131f95e986f6339c09411ae
rakin31 <shafqatrakin@gmail.com>
2022-08-14 00:59:59 +0600

    single_place_basic_done

---
+++ frontend/travello-frontend/src/SinglePlace/SinglePlace.js
@@ -0,0 +1,15 @@
+import "../App.css";
+import SinglePlaceDesc from "./SinglePlaceDesc";
+
+
+function SinglePlace() {
+  return (
+    <div className="body">
+      <SinglePlaceDesc />
+      {/*<ReviewBox />*/}
+      {/*<ShowReviews />*/}
+    </div>
+  );
+}
+
+export default SinglePlace;
---
+++ frontend/travello-frontend/src/SinglePlace/SinglePlaceDesc.js
@@ -0,0 +1,191 @@
+import "../App.css";
+import { SinglePlaceData } from "./SinglePlace-data";
+import React, { useEffect, useState } from "react";
+import "./SinglePlaceDesc.css";
+import SinglePlaceAPI from "./SinglePlaceAPI";
+import { Link } from "@mui/material";
+
+function SinglePlaceDesc() {
+  const [onePlace, setOnePlace] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [imgsrc, setImgsrc] = useState("");
+
+  useEffect(() => {
+    const api_path = "http://127.0.0.1:8000";
+    const explore_place = JSON.parse(localStorage.getItem("explore_place"));
+    if (explore_place !== null) {
+      //console.log("explore_place is not null");
+      //console.log(exploreSpot);
+      //const spot = await OnePlaceAPI.getOneSpotbyID(exploreSpot);
+      let my_place = [];
+      {
+        explore_place.map((r) =>
+          my_place.push({
+            id: r.place_id,
+            name: r.name,
+            short_description: r.short_description,
+            image: r.image,
+          })
+        );
+      }
+      //console.log(my_place);
+      //console.log(spot);
+      //console.log(my_place[0].id);
+      setOnePlace(my_place[0]);
+      setName(my_place[0].name);
+      setDescription(my_place[0].short_description);
+      setImgsrc(api_path + String(my_place[0].image));
+    } else {
+      console.log("explore place null found");
+    }
+
+    const exploreSpot = JSON.parse(localStorage.getItem("explore_spot"));
+    if (exploreSpot !== null) {
+      //console.log("spot is not null");
+      //console.log(exploreSpot);
+      //const spot = await OnePlaceAPI.getOneSpotbyID(exploreSpot);
+      let my_spot = [];
+      {
+        exploreSpot.map((r) =>
+          my_spot.push({
+            id: r.spot_id,
+            name: r.name,
+            short_description: r.short_description,
+            image: r.image,
+          })
+        );
+      }
+      //console.log(my_spot);
+      //console.log(spot);
+      setOnePlace(my_spot[0]);
+      setName(my_spot[0].name);
+      setDescription(my_spot[0].short_description);
+      setImgsrc(api_path + String(my_spot[0].image));
+    } else {
+      console.log("explore spot null found");
+    }
+
+    const p = JSON.parse(localStorage.getItem("place"));
+
+    if (p !== null) {
+      console.log("p is not null");
+      setOnePlace(p);
+      setName(p.name);
+      setDescription(p.short_description);
+      setImgsrc(api_path + p.image);
+      // OnePlaceAPI.updateImgPath(p.image);
+    }
+
+    const spot = JSON.parse(localStorage.getItem("spot"));
+    if (spot !== null) {
+      console.log("spot is not null");
+      //console.log(spot);
+      setOnePlace(spot);
+      setName(spot.name);
+      setDescription(spot.short_description);
+      setImgsrc(api_path + spot.image);
+    }
+
+    const food = JSON.parse(localStorage.getItem("food"));
+    if (food !== null) {
+      console.log("food is not null");
+      setOnePlace(food);
+      setName(food.food_name);
+      setDescription(food.short_description);
+      setImgsrc(api_path + food.image);
+    }
+  }, []);
+
+
+
+  return (
+    <div>
+      {/*<img src={imgsrc} alt="slide" className="imgdetails" />*/}
+
+      <div>
+        <img
+          className="img-fluid mainImage"
+          src={imgsrc}
+          alt="Responsive image"
+          width={"100%"}
+        />
+      </div>
+      {/*<div className="row">*/}
+      {/*  <div className="column" style={{ backgroundColor: "#bbb" }}>*/}
+      {/*    <Link*/}
+      {/*      href="/Comparison"*/}
+      {/*      onClick={() =>*/}
+      {/*        localStorage.setItem("load_category", JSON.stringify("place"))*/}
+      {/*      }*/}
+      {/*    >*/}
+      {/*      <img*/}
+      {/*        className="my_image"*/}
+      {/*        src={hotelimg}*/}
+      {/*        height={"50"}*/}
+      {/*        alt={"hotel"}*/}
+      {/*      />*/}
+      {/*    </Link>*/}
+      {/*    <img*/}
+      {/*      className="my_image"*/}
+      {/*      src={hotelimg}*/}
+      {/*      height={"50"}*/}
+      {/*      alt={"hotel"}*/}
+      {/*    />*/}
+      {/*    /!*<h2>Column 1</h2>*!/*/}
+      {/*    /!*<p>Some text..</p>*!/*/}
+      {/*  </div>*/}
+      {/*  <div className="column" style={{ backgroundColor: "#bbb" }}>*/}
+      {/*    <Link*/}
+      {/*      href="/Comparison"*/}
+      {/*      onClick={() =>*/}
+      {/*        localStorage.setItem("load_category", JSON.stringify("food"))*/}
+      {/*      }*/}
+      {/*    >*/}
+      {/*      <img*/}
+      {/*        className="my_image"*/}
+      {/*        src={foodimg}*/}
+      {/*        height={"50"}*/}
+      {/*        alt={"food"}*/}
+      {/*      />*/}
+      {/*    </Link>*/}
+
+      {/*    /!*<h2>Column 2</h2>*!/*/}
+      {/*    /!*<p>Some text..</p>*!/*/}
+      {/*  </div>*/}
+      {/*  <div className="column" style={{ backgroundColor: "#bbb" }}>*/}
+      {/*    <Link*/}
+      {/*      href="/Comparison"*/}
+      {/*      onClick={() =>*/}
+      {/*        localStorage.setItem("load_category", JSON.stringify("activity"))*/}
+      {/*      }*/}
+      {/*    >*/}
+      {/*      <img*/}
+      {/*        className="my_image"*/}
+      {/*        src={activityimg}*/}
+      {/*        height={"50"}*/}
+      {/*        alt={"activity"}*/}
+      {/*      />*/}
+      {/*    </Link>*/}
+
+      {/*    /!*<h2>Column 3</h2>*!/*/}
+      {/*    /!*<p>Some text..</p>*!/*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+      <div
+        style={{ marginTop: "20px", marginBottom: "20px", marginLeft: "10px" }}
+      >
+        <h2>
+          <b>
+            <u>{name}</u>
+          </b>
+        </h2>
+      </div>
+      <div>
+        <p style={{ marginLeft: "10px" }}>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SinglePlaceDesc;

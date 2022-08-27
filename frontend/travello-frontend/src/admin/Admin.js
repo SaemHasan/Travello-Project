6d7e8c58f31b7cc464f6f75dea3e20061149d549
SaemHasan <1705027@ugrad.cse.buet.ac.bd>
2022-08-27 15:14:25 +0600

    admin page update

--- frontend/travello-frontend/src/admin/Admin.js
+++ frontend/travello-frontend/src/admin/Admin.js
@@ -1,33 +1,62 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import APIService from "../APIService";
 import AdminInfo from "./AdminProfile";
 import AddInfo from "./AddInfo";
+import AdminSideBar from "./ShowSideBar";
+import AdminNavBar from "./AdminNavBar";
 
 function Admin() {
-  const [user, setUser] = useState({});
-  const user_image = "/assets/user.jpeg";
+    const [user, setUser] = useState({});
+    const [token, setToken] = useState(null);
+    const user_image = "/assets/user.jpeg";
+    const [showAddInfo, setShowAddInfo] = useState(false);
 
-  useEffect(() => {
-    const token = JSON.parse(localStorage.getItem("token"));
-    if (token) {
-      APIService.getUserObject(token)
-        .then(async (user) => {
-          await setUser(user);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, []);
-  return (
-    <div className="container">
-      <h1>Admin Page</h1>
 
-      <AdminInfo user={user} image={user_image} />
-      {/*<GridShow />*/}
-      <AddInfo />
-    </div>
-  );
+    useEffect(() => {
+        const token = JSON.parse(localStorage.getItem("token"));
+        if (token) {
+            setToken(token);
+            APIService.getUserObject(token)
+                .then(async (user) => {
+                    await setUser(user);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }, []);
+    return (
+        <>
+            <div className="container">
+                <h1 className="center_title">Admin Page</h1>
+                <div className="row col-6 center">
+                    {/*<AdminSideBar/>*/}
+                    <AdminNavBar setAddInfo={setShowAddInfo}/>
+                </div>
+                {
+                    !showAddInfo &&
+                    <div className="row">
+                        <div className="col-6 center">
+                            <AdminInfo user={user} image={user_image} token={token}/>
+                        </div>
+                    </div>
+
+                }
+                {
+                    showAddInfo &&
+                    <div className="row">
+                        <AddInfo/>
+                    </div>
+                }
+
+                {/*<GridShow />*/}
+
+
+            </div>
+
+        </>
+    )
+        ;
 }
 
 export default Admin;
---
+++ frontend/travello-frontend/src/admin/AdminNavBar.js
@@ -0,0 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function AdminNavBar(props) {
+    const addSpotClick = () => {
+        props.setAddInfo(true);
+        console.log("Add Spot Clicked");
+    }
+    const addHomeClick = () => {
+        props.setAddInfo(false);
+        console.log("Add Home Clicked");
+    }
+  return (
+    <div className="center">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand onClick={addHomeClick}>Home</Navbar.Brand>
+         <Navbar.Brand onClick={addSpotClick}>Add Data</Navbar.Brand>
+            <Navbar.Brand >Approve</Navbar.Brand>
+          <Navbar.Toggle />
+          {/*<Nav className="me-auto">*/}
+            {/*<Nav.Link href="#features">Features</Nav.Link>*/}
+            {/*<Nav.Link href="#pricing">Pricing</Nav.Link>*/}
+          {/*</Nav>*/}
+        </Container>
+      </Navbar>
+      <br />
+
+    </div>
+  );
+}
+
+export default AdminNavBar;
